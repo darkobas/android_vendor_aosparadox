ad5e8bb4786c5819b20b9a4ab64d7ab7d361a682
darkobas <darkobas@gmail.com>
2015-11-10 09:03:06 +0100

    my version addon

--- common.mk
+++ common.mk
@@ -84,5 +84,5 @@ PRODUCT_COPY_FILES += \
 endif
 
 # AOSParadox Version
-PAR_VERSION := $(shell date -u +%Y%m%d)
+PAR_VERSION := $(shell date -u +%Y%m%d)-$(DARKOBASV)
 PRODUCT_PROPERTY_OVERRIDES += ro.par.version =$(PAR_VERSION)
