aa50427bc6fff952928a0f5b3f6351bb2a1986c3
Marko Man <darkobas@gmail.com>
2016-04-01 00:13:10 +0200

    Revert "Revert "revert ffmpeg""
    This reverts commit cacd6a6171a9de4b579c3faa879b75e6dbf3a724.

--- common.mk
+++ common.mk
@@ -95,16 +95,6 @@ PRODUCT_COPY_FILES += \
     vendor/darkobas/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
     vendor/darkobas/prebuilt/common/bin/sysinit:system/bin/sysinit
 
-# Stagefright FFMPEG plugin
-PRODUCT_PACKAGES += \
-    libffmpeg_extractor \
-    libffmpeg_omx \
-    media_codecs_ffmpeg.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.sf.omx-plugin=libffmpeg_omx.so \
-    media.sf.extractor-plugin=libffmpeg_extractor.so
-
 # changelog copy
 PRODUCT_COPY_FILES += \
     Changelog.txt:system/etc/changelog.txt
